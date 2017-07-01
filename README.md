d1bec2c6c6fd9c24aba5ffac9d828f48f75357dd
Smykke <smykke@users.noreply.github.com>
2017-07-01 09:57:21 -0300

    Vários tratamento de erros
    - Comentários
    - Arquivo de chave vazio
    - Digitar uma letra que já foi descoberta
    - Digitar nada
    - Transformar todas as letras da chave em minúsculas
    - Chaves de mais de uma palavra

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 ### Linguagens de Programação
 
 #### Tarefas pendentes
-- [ ] Comentar o código
-- [ ] Checar se o arquivo está vazio
-- [ ] Adicionar suporte a chaves formadas por mais de uma palavra (ex.: "Homem-Aranha", "Homem de Ferro") / mostrar o espaço, hífen...
-- [ ] Decidir como tratar quando uma pessoa não digita nada (quando ainda resta uma chance ou quando está na última tentativa)
+- [ x ] Comentar o código
+- [ x ] Checar se o arquivo está vazio
+- [ x ] Adicionar suporte a chaves formadas por mais de uma palavra (ex.: "Homem-Aranha", "Homem de Ferro") / mostrar o espaço, hífen...
+- [ x ] Decidir como tratar quando uma pessoa não digita nada (quando ainda resta uma chance ou quando está na última tentativa)
+- [ x ] Se digitar uma letra repetida?
+- [ ] Acertar a falha na recursão quando digita uma letra repetida
+- [ ] Renomear o arquivo
--- hangman-lab.hs
+++ hangman-lab.hs
@@ -1,14 +1,30 @@
 import System.IO
+import System.Exit
+import Control.Monad (unless)
+import Data.Char (toLower)
 
+----- LER CHAVE: Lê a palavra contida no arquivo "chave.txt" e remove a quebra de linha.
+--Saída: chave (palavra a ser descoberta) [IO String]
 lerChave :: IO String
 lerChave = do
 	putStrLn "Lendo chave.txt"
 	original <- readFile "chave.txt"
-	return (take ((length original) -1) original) -- Tira o '\n'
+	if (length original) == 0
+		then do
+			putStrLn "Ops. Arquivo vazio!"
+			exitWith ExitSuccess
+		else return (take ((length original) -1) (map toLower original)) -- Tira o '\n'
 
+----- CODIFICAR: Recebe a palavra a ser descoberta e retorna uma string de underscores ("_")
+--com o mesmo tamanho da palavra.
+--Entrada: chave [String]
+--Saída: cadeia de underscores [String]
 codificar :: String -> String
-codificar palavra = [ '_' | p <- palavra]
+codificar palavra = [ if p == ' ' || p == '-' then p else '_' | p <- palavra]
 
+---- IMPRIMIR: Imprime uma palavra com um espaço entre as letras
+--Entrada: palavra [String]
+--Saída: imprime o caractere na tela [IO ()]
 imprimir :: String -> IO ()
 imprimir [] = putChar '\n'
 imprimir (x:xs) = do
@@ -16,37 +32,50 @@ imprimir (x:xs) = do
 	putChar ' '
 	imprimir xs
 
-tamanho :: String -> Int
-tamanho palavra = length palavra
+---- RESULTADO FINAL: Imprime se o jogador acertou ou não
+acertou :: String -> IO()
+acertou chave = putStrLn ("Resposta correta - Vitória!\n" ++ chave)
 
+errou :: IO()
+errou = putStrLn "Resposta incorreta - Derrota!"
+
+---- ADIVINHAR: Corresponde ao loop principal do jogo. Lê a entrada do usuário,
+--compara com a chave e o conduz a uma nova tentativa ou mostra o resultado final.
+--Entrada: chava [String], sequência com letras que o usuário já adivinhou +
+--underscores [String] e número da tentativa atual [Int]
+--Saída: resultado final [IO ()]
 adivinhar :: String -> String -> Int -> IO ()
 adivinhar chave tentativa count = do
 	putStr "\nPrograma: "
 	imprimir tentativa
-	putStr "Jogador ("
-	if count < 8 then putStr $ show count else putStr "ultima chance): "
-	putStr "): "
+	putStr ("Jogador (" ++ (if count < 8 then show (count) else "ultima chance") ++ "): ")
+--where
+
 	palavra <- getLine
-
-	if (length palavra) == 1
+ -- verificar se está na púltima tentativa => só pode verificar a palavra inteira e não uma letra
+	if (length palavra) == 1 -- Usuário digitou uma letra
 		then do
-			let letra = head palavra
-			let nova_tentativa =	[ if x == letra then x else y | (x, y) <- zip chave tentativa]
+			let letra = toLower(head palavra)
+			let nova_tentativa = [ if x == letra then x else y | (x, y) <- zip chave tentativa]
+			unless (not(elem letra tentativa)) $ do
+				putStrLn "Hm, parece que você já digitou essa letra"
+				adivinhar chave tentativa count   -- tem que ser chamado tudo junto no fim ou colocar um exit ao sair (ExitSuccess)
 			if chave == nova_tentativa
-				then putStrLn chave ++ "Resposta correta - Vitória!"
-				else if count < 8 then adivinhar chave nova_tentativa (count+1)
-					else putStrLn "Resposta incorreta - Derrota!"
-		else if (length palavra) == (length chave)
-			then do
-				if chave == palavra
-					then putStrLn "Resposta correta - Vitória!"
-					else putStrLn "Resposta incorreta - Derrota!"
-		else putStrLn "Resposta incorreta - Derrota!"
-
+				then acertou chave
+				else if count < 8
+					then adivinhar chave nova_tentativa (count+1)
+					else errou
+		else if (length palavra) == (length chave) -- Usuário tentou adivinhar a palavra
+			then if chave == palavra
+				then acertou chave
+				else errou
+			else if (length palavra) == 0 --
+				then adivinhar chave tentativa count
+				else errou -- Usuário não inseriu uma tentativa válida
 
 main :: IO ()
 main = do
 	chave <- lerChave
 --	putStrLn chave
 	adivinhar chave (codificar chave) 1
-	putStrLn "\nQue pessoa inteligente!"
+	putStrLn ("\nElementar, meu caro Watson. Você já exercitou " ++ show(length(chave)) ++ " neurônios hoje.")
